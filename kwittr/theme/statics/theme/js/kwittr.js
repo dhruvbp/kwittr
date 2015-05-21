a3b1070f2f37a93b648cf1904dc957365eece571
Dhruv <dhruv.b.patel@hiof.no>
2015-05-21 13:00:23 +0200

    opprettet en mappe som heter statics i theme for js , hvor du klikker for og gi likes

---
+++ kwittr/theme/statics/theme/js/kwittr.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function(){
+
+    $(".add-points-link").click(function(event) {
+        event.preventDefault();
+        var note_id = $(this).data("noteid");
+        $.ajax({
+            url: $('#add_points_url').val() + "/" + note_id
+        })
+        .done(function(data){
+            var points_updated = data['points_updated'];
+            var points_element_id = "#id-points-for-note-" + note_id;
+            $(points_element_id).html(points_updated);
+        });
+    });
+
+    $("#increase_passed_exams_button").click(function(event) {
+        event.preventDefault();
+        $.ajax({
+            url: $('#passed_exams_url').val()
+        })
+        .done(function(data){
+            var passed_exams_updated = data['passed_exams_updated'];
+            $("#passed_exams_cell").html(passed_exams_updated);
+        });
+    });
+
+
+
+
+
+
+});
