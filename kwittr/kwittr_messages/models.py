fb151ab888e31809def96a8ed66ad31a93417a81
Lars Torgersen <larstorgersen100@hotmail.com>
2015-05-22 10:32:52 +0200

    Vi har laget class message for å lage en modell for meldinger

--- kwittr/kwittr_messages/models.py
+++ kwittr/kwittr_messages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Message(models.Model):
+    message = models.TextField()
+    likes = models.PositiveIntegerField(default=0)
+    created_by = models.ForeignKey(User, related_name="created_message")
+    created_datetime = models.DateTimeField()
+
+    def __unicode__(self):
+        return u'%s' % self.message
+
+    class Meta:
+        ordering = ['-id']
+
