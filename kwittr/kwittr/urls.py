b8a36fc79867ff9dc0b3f1ce9f350741534d16e6
Lars Torgersen <larstorgersen100@hotmail.com>
2015-05-22 10:29:56 +0200

    Vi har lagt til url for messages slik at den tar med deg videre til message views.py

--- kwittr/kwittr/urls.py
+++ kwittr/kwittr/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', include('theme.urls')),
     url(r'^useraccounts/', include('useraccounts.urls')),
+    url(r'^messages/', include('kwittr_messages.urls')),
 ]
