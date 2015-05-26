ef24174ca7d424fb2fee98f541485744ae6a0e53
Dhruv <dhruv.b.patel@hiof.no>
2015-05-26 11:31:34 +0200

    vi en ny if for at den skal sjekke om brukerenavne alrede existerer

--- kwittr/useraccounts/views.py
+++ kwittr/useraccounts/views.py
@@ -26,14 +26,18 @@ def user_logout(request):
 def user_register(request):
     context = {}
     if request.method == "POST":
-        user = User()
-        user.first_name = request.POST.get('firstname')
-        user.last_name = request.POST.get('lastname')
-        user.username = request.POST.get('username')
-        user.email = request.POST.get('email')
-        user.set_password(request.POST.get('password'))
-        user.save()
-        context['user_saved_successfully'] = True
+        if User.objects.get(username=request.POST.get('username')):
+            context['username_allready_exist'] = True
+            return render(request, 'useraccounts/register.html', context)
+        else:
+            user = User()
+            user.first_name = request.POST.get('firstname')
+            user.last_name = request.POST.get('lastname')
+            user.username = request.POST.get('username')
+            user.email = request.POST.get('email')
+            user.set_password(request.POST.get('password'))
+            user.save()
+            context['user_saved_successfully'] = True
     return render(request, 'useraccounts/register.html', context)
 
 
