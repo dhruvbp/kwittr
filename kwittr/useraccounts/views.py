5c91aaff45245597838cfc4d3cf60ddbcb8d8517
Dhruv <dhruv.b.patel@hiof.no>
2015-06-01 11:08:05 +0200

    Laget views for editering av profile

--- kwittr/useraccounts/views.py
+++ kwittr/useraccounts/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+
 
 def user_login(request):
     context = {}
@@ -26,21 +28,37 @@ def user_logout(request):
 def user_register(request):
     context = {}
     if request.method == "POST":
-        if User.objects.get(username=request.POST.get('username')):
-            context['username_allready_exist'] = True
+        new_user = request.POST.get('username').lower()
+        if User.objects.filter(username=new_user).exists():
+            context['username_already_exist'] = True
             return render(request, 'useraccounts/register.html', context)
-        else:
-            user = User()
-            user.first_name = request.POST.get('firstname')
-            user.last_name = request.POST.get('lastname')
-            user.username = request.POST.get('username')
-            user.email = request.POST.get('email')
-            user.set_password(request.POST.get('password'))
-            user.save()
-            context['user_saved_successfully'] = True
+        new_email = request.POST.get('email')
+        if User.objects.filter(email=new_email).exists():
+            context['email_already_exist'] = True
+            return render(request, 'useraccounts/register.html', context)
+        
+        user = User()
+        user.first_name = request.POST.get('firstname')
+        user.last_name = request.POST.get('lastname')
+        user.username = new_user
+        user.email = new_email
+        user.set_password(request.POST.get('password'))
+        user.save()
+        context['user_saved_successfully'] = True
     return render(request, 'useraccounts/register.html', context)
 
 
+@login_required
+def user_profile(request):
+    context = {}
+    if request.method == "POST":
+        user = request.user
+        user.first_name = request.POST.get('firstname')
+        user.last_name = request.POST.get('lastname')
+        user.email = request.POST.get('email')
+        user.save()
+        context['user_saved_successfully'] = True
+    return render(request, 'useraccounts/profile.html', context)
 
 
 
