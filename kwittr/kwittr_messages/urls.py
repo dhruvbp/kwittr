2eda40b01ab1957835f6e1a314cfceb612cdcc83
Lars Torgersen <larstorgersen100@hotmail.com>
2015-06-02 10:37:13 +0200

    opprydding i urls koden

--- kwittr/kwittr_messages/urls.py
+++ kwittr/kwittr_messages/urls.py
@@ -4,9 +4,7 @@ from kwittr_messages import views
 
 urlpatterns = patterns('',
     url(r'^$', views.message_listing, name='message_listing'),
-    url(r'^(\d+)/$', views.message_details, name='message_details'),
-   # url(r'^(\d+)/increase_message_likes$', views.message_likes, 
-        #name='add_message_likes'),   
+    url(r'^(\d+)/$', views.message_details, name='message_details'),  
     url(r'^message_add_likes/(\d+)$', views.message_add_likes, 
         name='message.likes'),  
      
